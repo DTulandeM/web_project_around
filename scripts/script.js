e49aa0a34b290dd1597058f16a046c9f6025ec70
dtulande <dtulande@gmail.com>
2024-04-27 19:22:19 -0500

    Proyecto final

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,31 @@
 const popupProfile = document.querySelector("#menupopup-profile");
 const profileButton = document.querySelector(".profile__edit-button");
 const closeProfileButton = document.querySelector(".menupopup__button-close");
-let formElementName = document.querySelector("#profile-name");
-let formElementAboutme = document.querySelector("#profile-aboutme");
-
-console.log(formElementName);
+const saveProfileButton = document.querySelector(".form__button-save");
+const formProfile = document.querySelector("#form-profile");
+const overlay = document.querySelector("#cover");
+let formProfileName = document.querySelector(".profile__name");
+let formProfileAboutme = document.querySelector(".profile__about-me");
+let formInputName = document.querySelector("#profile-name");
+let formInputAboutme = document.querySelector("#profile-aboutme");
 
 function showPopupProfile() {
   popupProfile.classList.add("menupopup__profile-show");
+  overlay.classList.add("menupopup__overlay");
+  formInputName.value = formProfileName.textContent;
+  formInputAboutme.value = formProfileAboutme.textContent;
 }
 function closePopupProfile() {
   popupProfile.classList.remove("menupopup__profile-show");
+  overlay.classList.remove("menupopup__overlay");
 }
+
+formProfile.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  formProfileName.textContent = formInputName.value;
+  formProfileAboutme.textContent = formInputAboutme.value;
+  closePopupProfile();
+});
 
 profileButton.addEventListener("click", showPopupProfile);
 closeProfileButton.addEventListener("click", closePopupProfile);
